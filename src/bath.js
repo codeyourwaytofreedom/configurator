09dd15b23b6d2c828209f21c7c33254bcbd4b88e
codeyourwaytofreedom <can.kolej@gmail.com>
2022-09-14 14:25:29 +0300

    First job First commit Bathroom configurator

--- src/bath.js
+++ src/bath.js
@@ -1,8 +1,7 @@
 import bath from './images/one.png';
 import './bath.css';
 import { useState } from 'react';
-import riva from './images/Griffmulde/Riva-Roxy-893-Eiche-San-Remo.png';
-
+import {Front_Products, Korpus_Products,Waschtisch_Products} from './photo_importer';
 
 
 const Bath = () => {
@@ -11,34 +10,27 @@ const Bath = () => {
     const [Korpus, setKorpus] = useState(false)
     const [Waschtisch, setWaschtisch] = useState(false)
 
-    const Front_Products = [{name:"Weiß Softmatt", image:require('./images/Front/weiss-softmatt.png')},
-    {name:"Ston", image:require('./images/Front/Ston-F303.png')},
-    {name:"Weiß hochglanz", image:require('./images/Front/Weiss-hochglanz.png')},
-    {name:"Magnolia Softmatt", image:require('./images/Front/magnolia-softmatt.png')},
-    {name:"Riva Roxy Eiche San Remo", image:require('./images/Front/Riva-Roxy-893-Eiche-San-Remo.png')},
-    {name:"Magnolia hochglanz", image:require('./images/Front/Magnolia-hochglanz.png')},
-    {name:"Seidengrau hochglanz", image:require('./images/Front/Seidengrau-hochglanz.png')},
-    {name:"Focus Fire Schwarz", image:require('./images/Front/Focus-Fire-472-Schwarz-HGL-D01.png')},
-    {name:"Fontana", image:require('./images/Front/Fontana-F453-F01.png')}]
     
-    const [show_image, setShow] = useState(false)
+    const [show_image_front, setShow_front] = useState(false)
+    const [ima_front, setImage_front] = useState(null)
 
-    function image_handler(){
-        if (!show_image)
-        {setShow(true)}
+    const [show_image_korpus, setShow_korpus] = useState(false)
+    const [ima_korpus, setImage_korpus] = useState(null)
+
+    function image_handler_front(a){
+        setShow_front(true)
+        setImage_front(a)
     }
-    // ,
-    //                         , "" ,"", "Seidengrau","Seidengrau hochglanz",
-    //                         "Sand" , "Sand Hochglanz" ,"Beton grau", "Schiefer Steingrau", "Steingrau Matt" ,
-    //                         "Schiefergrau", "Schwarzbeton" ]
 
-    // const Korpus_Products = ["Weiß" ,"Magnolia Matt" , "Sand","Seidengrau"  ,"Schiefergrau"
-    //                         ,"Eiche Virginia","Eiche San Remo" ]
+    function image_handler_korpus(a){
+        setShow_korpus(true)
+        setImage_korpus(a)
+        console.log("clicked korpus")
+    }
 
-    // const Waschtisch_Products = ["Alpinweiß" ,"Seidengrau" , "Beton Grau","Beton Schiefergrau"  
-    // ,"Dekor Oxid","Dekor Metal Art","Granit Schwarz geflammt","Schiefer" ,"Schiefergrau",
-    // "Copper Vintage","Steel Vintage", "Eiche Halifax","Eiche Virginia","Eiche San Remo",
-    //   "Endgrain Oak","Eiche Yukon", "Taxus", "Arizona Pine"]
+
+
+
 
     
       function front_mouse_enter () {
@@ -62,8 +54,11 @@ const Bath = () => {
             <div className="bath_panel">
                 <h1 style={{position:"absolute",top:"-10%", left:"40%" }}>Dein Haus! Deine Seele </h1>
                 <img className='bath_image' src={bath} alt="bath_image_alternative" />
-                <img className='test' style={{position:"absolute", left:"0.25%", top:"-0.9%" ,width:"1153px", 
-                height:"600px",display:show_image ? "block": "none"}} src={riva} alt="bath_image_alternative" />
+                <img className='test' style={{position:"absolute", left:"0%", top:"0%" ,width:"100%", 
+                height:"100%",display:show_image_front ? "block": "none"}} src={ima_front} alt="bath_image_alternative" />
+
+                <img className='korpus' style={{position:"absolute", left:"0%", top:"0%" ,width:"100%", 
+                height:"100%",display:show_image_korpus ? "block": "none"}} src={ima_korpus} alt="bath_image_alternative" />
 
                 <div onMouseLeave={()=> setDetails(false)} className="bath_panel_shell">
                    
@@ -77,34 +72,36 @@ const Bath = () => {
                         </div>
 
                     
-                        <div style={{display: Details ? "grid" : "none", height: Front || Waschtisch ? "100%" : "50%",
-                                marginTop: Korpus ? "20%" : "0"}} className="bath_panel_options_detail">                   
+                        <div style={{display: Details ? "grid" : "none",
+                               }} className="bath_panel_options_detail">                   
                             { Front &&
                                 Front_Products.map((F) =>
                                 // console.log(F.image) &&
                                 <div  key={F.name} className="option_in_subcategory" onClick={() => setDetails(false)}>
-                                    <img onClick={() => image_handler(F.image) } className='product_image' style={{width:"100%", height:"100%"}} src={F.image} alt="Image" /> 
+                                    <img onClick={() => image_handler_front(F.code) } className='product_image' style={{width:"100%", height:"100%"}} src={F.image} alt="Image" /> 
                                     <div className="product_name">{F.name}</div> 
                                 </div>
                                 )
                             }
-                            {/* { Korpus && 
+                            
+                            { Korpus && 
                                 Korpus_Products.map((F) =>
-                                <div key={F} className="option_in_subcategory">
-                                   <img className='product_image' alt="Image" />
-                                   <div className="product_name">{F}</div> 
+                                <div key={F.name} className="option_in_subcategory">
+                                   <img onClick={() => image_handler_korpus(F.code) } className='product_image' src={F.image} style={{width:"90%", height:"90%"}} alt="Image" />
+                                   <div className="product_name">{F.name}</div> 
                                 </div>
                                 )
 
                             }
+
                             { Waschtisch && 
                                 Waschtisch_Products.map((F) =>
-                                <div key={F} className="option_in_subcategory">
-                                    <img className='product_image' alt="Image" />
-                                    <div className="product_name">{F}</div> 
+                                <div key={F.name} className="option_in_subcategory">
+                                    <img className='product_image' src={F.image} alt="Image" style={{width:"100%", height:"100%"}}/>
+                                    <div className="product_name">{F.name}</div> 
                                 </div>
                                 )
-                            } */}
+                            }
                                  
 
 
---
+++ src/photo_importer.js
@@ -0,0 +1,175 @@
+
+const Front_Products = [
+{name:"Weiß Softmatt", 
+code:require('./images/Front/weiss-softmatt.png'), 
+image:require('./images/Front_Thumbnail/weiss-softmatt.png')},
+
+{name:"Ston",
+code:require('./images/Front/Ston-F303.png'), 
+image:require('./images/Front_Thumbnail/Ston-F303.png')},
+
+{name:"Weiß hochglanz", 
+code:require('./images/Front/Weiss-hochglanz.png'), 
+image:require('./images/Front_Thumbnail/Weiss-hochglanz.png')},
+
+{name:"Magnolia Softmatt", 
+code:require('./images/Front/Magnolia-softmatt.png'), 
+image:require('./images/Front_Thumbnail/magnolia-softmatt.png')
+},
+
+{name:"Riva Roxy Eiche San Remo", 
+code:require('./images/Front/Riva-Roxy-893-Eiche-San-Remo.png'), 
+image:require('./images/Front_Thumbnail/Riva-Roxy-893-Eiche-San-Remo.png')},
+
+{name:"Magnolia hochglanz", 
+code:require('./images/Front/Magnolia-hochglanz.png'), 
+image:require('./images/Front_Thumbnail/Magnolia-hochglanz.png')},
+
+{name:"Seidengrau hochglanz", 
+code:require('./images/Front/Seidengrau-hochglanz.png'), 
+image:require('./images/Front_Thumbnail/Seidengrau-hochglanz.png')},
+
+{name:"Focus Fire Schwarz", 
+code:require('./images/Front/Focus-Fire-472-Schwarz-HGL-D01.png'), 
+image:require('./images/Front_Thumbnail/Focus-Fire-472-Schwarz-HGL-D01.png')},
+
+{name:"Fontana", 
+code:require('./images/Front/Fontana-F453-F01.png'), 
+image:require('./images/Front_Thumbnail/Fontana-F453-F01.png')}
+]
+
+
+
+
+const Korpus_Products = [
+
+    {name:"Premium Weiß",
+    image:require('./images/Korpus_Thumbnail/Premiumweiss.png'),
+    code:require('./images/Korpus/Premiumweiss.png')
+    },
+    
+    {name:"Magnolia Matt",
+    image:require('./images/Korpus_Thumbnail/Magnolia-matt.png'),
+    code:require('./images/Korpus/Magnolia-matt.png')
+    }, 
+    
+    {name:"Sand",
+    image:require('./images/Korpus_Thumbnail/Speed-Sonja-271-sand.png'),
+    code:require('./images/Korpus/Speed-Sonja-271-sand.png')
+    },              
+    
+    {name:"Seidengrau",
+    image:require('./images/Korpus_Thumbnail/Seidengrau.png'),
+    code:require('./images/Korpus/Seidengrau.png')
+    },  
+                        
+    {name:"Schiefergrau",
+    image:require('./images/Korpus_Thumbnail/Schiefergrau.png'),
+    code:require('./images/Korpus/Schiefergrau.png')
+    },
+    
+    {name:"Eiche Virginia",
+    image:require('./images/Korpus_Thumbnail/Eiche-Virginia-NB.png'),
+    code:require('./images/Korpus/Eiche-Virginia-NB.png')
+    },
+    
+    {name:"Eiche San Remo",
+    image:require('./images/Korpus_Thumbnail/Eiche-San-Remo-NB.png'),
+    code:require('./images/Korpus/Eiche-San-Remo-NB.png')
+    } ]
+
+           
+
+
+    const Waschtisch_Products = [
+        {name:"Alpinweiß",
+        image:require('./images/APL_Thumbnail/Alpinweiß.png'),
+        code:require('./images/APL/Alpinweiß.png')
+        } ,
+
+        {name:"Seidengrau",
+        image:require('./images/APL_Thumbnail/Seidengrau.png'),
+        code:require('./images/APL/Seidengrau.png')
+        } , 
+        
+        {name:"Beton Grau",
+        image:require('./images/APL_Thumbnail/Beton-grau-NB.png'),
+        code:require('./images/APL/Beton-grau-NB.png')
+        },
+
+        {name:"Beton Schiefergrau",
+        image:require('./images/APL_Thumbnail/Beton-Schiefergrau.png'),
+        code:require('./images/APL/Beton-Schiefergrau.png')
+        },  
+
+        {name:"Dekor Oxid",
+        image:require('./images/APL_Thumbnail/Dekor-Oxid.png'),
+        code:require('./images/APL/Dekor-Oxid.png')
+        },
+
+        {name:"Dekor Metal Art",
+        image:require('./images/APL_Thumbnail/Dekor-Metal-Art.png'),
+        code:require('./images/APL/Dekor-Metal-Art.png')
+        },
+
+        {name:"Granit Schwarz geflammt",
+        image:require('./images/APL_Thumbnail/Granit-schwarz-geflammt-NB.png'),
+        code:require('./images/APL/Granit-schwarz-geflammt-NB.png')
+        },
+
+        {name:"Schiefer",
+        image:require('./images/APL_Thumbnail/Schiefer-NB.png'),
+        code:require('./images/APL/Schiefer-NB.png')
+        },
+
+        {name:"Schiefergrau",
+        image:require('./images/APL_Thumbnail/Schiefergrau.png'),
+        code:require('./images/APL/Schiefergrau.png')
+        },
+
+        {name:"Copper Vintage",
+        image:require('./images/APL_Thumbnail/Copper-Vintage-NB.png'),
+        code:require('./images/APL/Copper-Vintage-NB.png')
+        },
+
+        {name:"Steel Vintage",
+        image:require('./images/APL_Thumbnail/Steel-Vintage-NB.png'),
+        code:require('./images/APL/Steel-Vintage-NB.png')
+        }, 
+
+        {name:"Eiche Halifax",
+        image:require('./images/APL_Thumbnail/Eiche-Halifax-NB.png'),
+        code:require('./images/APL/Eiche-Halifax-NB.png')
+        },
+
+        {name:"Eiche Virginia",
+        image:require('./images/APL_Thumbnail/Eiche-Virginia-NB.png'),
+        code:require('./images/APL/Eiche-Virginia-NB.png')
+        },
+
+        {name:"Eiche San Remo",
+        image:require('./images/APL_Thumbnail/Eiche-San-Remo-NB.png'),
+        code:require('./images/APL/Eiche-San-Remo-NB.png')
+        },
+
+        {name:"Endgrain Oak",
+        image:require('./images/APL_Thumbnail/Endgrain-Oak-NB.png'),
+        code:require('./images/APL/Endgrain-Oak-NB.png')
+        },
+
+        {name:"Eiche Yukon",
+        image:require('./images/APL_Thumbnail/Eiche-Yukon-NB.png'),
+        code:require('./images/APL/Eiche-Yukon-NB.png')
+        }, 
+
+        {name:"Taxus",
+        image:require('./images/APL_Thumbnail/Taxus-NB.png'),
+        code:require('./images/APL/Taxus-NB.png')
+        }, 
+
+        {name:"Arizona Pine",
+        image:require('./images/APL_Thumbnail/Arizona-Pine-NB.png'),
+        code:require('./images/APL/Arizona-Pine-NB.png')
+        }]                 
+
+export {Front_Products,Korpus_Products,Waschtisch_Products};
